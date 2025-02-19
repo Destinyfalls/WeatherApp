9a336b19cb27e1e4c68a5c4a6e761770a4df96e4
Igor Bilobrov <73975521+Destinyfalls@users.noreply.github.com>
2025-02-19 03:23:33 +0200

    Initial setup

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Igor Belobrov on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Igor Belobrov on 19.02.2025.
+//
+
+import Testing
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
